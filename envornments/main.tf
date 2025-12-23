97c5cdf0b2c1c537639299629512a8d244b4ff4a
swapnil <swapnil.jumbale@gmail.com>
2025-12-23 21:20:42 +0530

    cherry pickup

--- envornments/main.tf
+++ envornments/main.tf
@@ -14,6 +14,12 @@ module "azurerm_resource_group" {
     azurerm_resource_group_location = "central india"
 }
 
+module "azurerm_resource_group" {
+    source = "../modules/azurerm_resource_group"
+    azurerm_resource_group_name = "infinite_rg4"
+    azurerm_resource_group_location = "central india"
+}
+
 module "azurerm_virtual_network" {
     depends_on = [ module.azurerm_resource_group ]
     source = "../modules/azurerm_virtual_network"
